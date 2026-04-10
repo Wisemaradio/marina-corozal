a65a721d5e7534c85f02ede74bcee1aea4e62c28
Wisemaradio <wisema2@hotmail.com>
2026-04-10 12:20:30 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'marina-corozal-v1';
+
+// INSTALAR
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+// ACTIVAR
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener('fetch', event => {
+
+  const url = event.request.url;
+
+  // ❌ NO tocar streaming ni metadata
+  if (
+    url.includes('/listen/') ||
+    url.includes('/api/') ||
+    url.includes('.mp3') ||
+    url.includes('nowplaying')
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+
+});
